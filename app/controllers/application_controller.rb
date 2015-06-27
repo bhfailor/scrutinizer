84bab302ead930d1d680518f4a8439480f7ae115
Bruce Failor <bruce.failor@gmail.com>
2015-06-27 18:08:32 +0000

    authenticate api-key

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::API
+  attr_reader :current_consumer
+  before_action :set_consumer
+
+  private
+  
+  def set_consumer
+    @current_consumer = APIKey.find_by(key: params['api-key']).try(:consumer)
+    if @current_consumer.nil?
+      render json: { error: 'Unauthorized' }, status: 401
+      return
+    end
+  end
+  
 end
--- test/integration/rundowner_can_add_recipe_account_test.rb
+++ test/integration/rundowner_can_add_recipe_account_test.rb
@@ -4,10 +4,6 @@ require 'pry'
 
 class RundownerCanAddRecipeAccountTest < ActionDispatch::IntegrationTest
   let(:key_value) { 'somethingrandom' }
-  let (:rundowner_key) { APIKey.new(key: key_value, consumer: 'rundowner') }
-  before do
-    rundowner_key.save!
-  end
   let (:base_url) { '/api/v1/recipe_accounts?' }
   let (:authentication) { "&api-key=#{key_value}" }
   let (:the_data) do
@@ -15,14 +11,40 @@ class RundownerCanAddRecipeAccountTest < ActionDispatch::IntegrationTest
                         'login_username' => 'foo.bar@baz.com',
                         'login_password' => 'top_sekret')
   end
-  
-  test 'validating the consumer' do
-    assert key_value == rundowner_key.key
+
+  class UnauthenticatedConsumer < RundownerCanAddRecipeAccountTest
+    let (:bogus_authentication) { "&api-key=#{'bogus'}" }
+    let (:assert_unauthorized) do
+      lambda do
+        assert response.status == 401
+        assert JSON.parse(response.body) == { 'error' => 'Unauthorized' }
+      end
+    end
+        
+    test 'authenticating a request without an api key' do
+      post  URI.parse(base_url + the_data)
+      assert_unauthorized.call
+    end
+
+    test 'authenticating a request with bogus api key' do
+      post URI.parse(base_url + the_data + bogus_authentication)
+      assert_unauthorized.call
+    end
+    
   end
-  test 'adding a recipe account' do
-    assert RecipeAccount.count == 0
-    post URI.parse(base_url + the_data + authentication)
-    assert RecipeAccount.count == 1
-  end
   
+  class AuthenticatedConsumer < RundownerCanAddRecipeAccountTest
+  let (:rundowner_key) { APIKey.new(key: key_value, consumer: 'rundowner') }
+
+    before do
+      rundowner_key.save!
+    end
+
+    test 'adding a recipe account' do
+      assert RecipeAccount.count == 0
+      post URI.parse(base_url + the_data + authentication)
+      assert RecipeAccount.count == 1
+    end
+  end
+
 end
